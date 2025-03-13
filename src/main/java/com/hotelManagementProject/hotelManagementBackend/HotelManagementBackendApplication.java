1a54f9dfa24d940d2c4e8de495799b7cf607312f
asohyannick <asohyannick3@gmail.com>
2025-03-13 12:16:13 -0700

    I have successfully developed my Spring Boot REST API template with the neccessary third-party APIs and dependencies for my hotel management API project

---
+++ src/main/java/com/hotelManagementProject/hotelManagementBackend/HotelManagementBackendApplication.java
@@ -0,0 +1,15 @@
+package com.hotelManagementProject.hotelManagementBackend;
+import org.springframework.boot.SpringApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class HotelManagementBackendApplication {
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+		System.setProperty("SPRING_APPLICATION_PORT", dotenv.get("SPRING_APPLICATION_PORT"));
+		SpringApplication.run(HotelManagementBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hotelManagementProject/hotelManagementBackend/HotelManagementBackendApplicationTests.java
@@ -0,0 +1,10 @@
+package com.hotelManagementProject.hotelManagementBackend;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+class HotelManagementBackendApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+
+}
